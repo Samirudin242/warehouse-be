c1941d4e27a8c1335ad8b78d2a72b384afd8a042
Samirudin242 <samtheyaku@gmail.com>
2025-01-07 11:42:04 +0700

    update entity product service

--- product-service/product-dataaccess/src/main/java/com/fns/product/service/dataaccess/entity/LocationEntity.java
+++ product-service/product-dataaccess/src/main/java/com/fns/product/service/dataaccess/entity/LocationEntity.java
@@ -1,4 +1,42 @@
 package com.fns.product.service.dataaccess.entity;
 
-public class LocationEntity {
+import com.fns.domain.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "location")
+@Entity
+public class LocationEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "uuid2")
+    private UUID id;
+
+    private String address;
+
+    private String province;
+
+    private String city;
+
+    private String province_id;
+
+    private String city_id;
+
+    private String postal_code;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private UserEntity users;
+
+    @OneToOne
+    @JoinColumn(name = "warehouse_id", referencedColumnName = "id")
+    private WarehouseEntity warehouses;
 }
--- product-service/product-dataaccess/src/main/java/com/fns/product/service/dataaccess/entity/WarehouseEntity.java
+++ product-service/product-dataaccess/src/main/java/com/fns/product/service/dataaccess/entity/WarehouseEntity.java
@@ -1,4 +1,29 @@
 package com.fns.product.service.dataaccess.entity;
 
-public class WarehouseEntity {
+import com.fns.domain.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "warehouse")
+@Entity
+public class WarehouseEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "uuid2")
+    private UUID id;
+
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "admin_id", referencedColumnName = "id")
+    private UserEntity user;
 }
+
